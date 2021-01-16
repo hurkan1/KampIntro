83b2f7872b481e4b93a0ee27cbc579cc9893bfc3
Hürkan Mustafa AYDIN <huaydin@innova.com.tr>
2021-01-16 23:46:53 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety - tip güvenliği
+            // Do not repeat yourself - Kendini tekrarlama
+
+            // değer tutucu, alias
+            string katagoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolardun = 7.35;
+            double dolarbugun = 7.45;
+
+            if (sistemeGirisYapmisMi)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            if (dolardun>dolarbugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolardun < dolarbugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+            //Console.WriteLine(katagoriEtiketi);
+
+
+
+
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kurs1 = "yazilim gelistirici yetiştirme kampi";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+
+            //aarray - dizi
+
+
+            string[] kurslar = new string[] { "yazilim gelistirici yetiştirme kampi",
+            "Programlamaya başlangıç için temel kurs",
+            "java","Python"};
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Sayfa Sonu");
+        }
+    }
+}
